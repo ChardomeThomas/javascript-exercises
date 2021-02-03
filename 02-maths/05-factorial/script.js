4949ca40af4cf91a1bf42cf1ac3939bdfd059108
ChardomeThomas <thomas.chardome@gmail.com>
2021-02-03 15:25:55 +0100

    change in js

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,22 +1,30 @@
 /* becode/javascript
- *
- * /02-maths/05-factorial/script.js - 2.5: Factorielle
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+*
+* /02-maths/05-factorial/script.js - 2.5: Factorielle
+*
+* coded by leny@BeCode
+* started at 26/10/2018
+*/
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
 (function() {
-    
-    // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function() {
+  // to get the value of an input: document.getElementById("element-id").value
 
-        // your code here
+  document.getElementById("run").addEventListener("click", function() {
 
-    });
+    // your code here
+
+    var nb = document.getElementById("number").value;
+    var i = 1;
+    var res=1;
+    while (i<=nb){
+      res = res*i;
+      i++;
+    }
+    alert("la factorielle de " +  nb + " : "  + res);
+  });
 
 })();
