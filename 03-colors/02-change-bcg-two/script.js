366783b5ffa8f75e0280843fe80a04101ab30de5
ChardomeThomas <thomas.chardome@gmail.com>
2021-02-04 10:21:15 +0100

    change in js

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -12,5 +12,8 @@
 (function() {
 
     // your code here
-
+    document.getElementById("run").addEventListener("click", function() {
+    var col= document.getElementById("color").value;
+    document.body.style.backgroundColor = col;
+    });
 })();
