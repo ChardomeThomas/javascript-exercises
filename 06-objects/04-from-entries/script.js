f7ce9cd7200492da84bb05f46908d26999ec2337
ChardomeThomas <thomas.chardome@gmail.com>
2021-02-10 14:32:05 +0100

    change in js

--- 06-objects/04-from-entries/script.js
+++ 06-objects/04-from-entries/script.js
@@ -1,16 +1,25 @@
 /* becode/javascript
- *
- * /06-objects/04-from-entries/script.js - 6.4: fromEntries
- *
- * coded by leny@BeCode
- * started at 08/05/2019
- */
+*
+* /06-objects/04-from-entries/script.js - 6.4: fromEntries
+*
+* coded by leny@BeCode
+* started at 08/05/2019
+*/
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
 (() => {
-    const keys = ["name", "species", "age", "gender", "color"];
-    const values = ["Skitty", "cat", 9, "female", "tabby"];
-    // your code here
+  const keys = ["name", "species", "age", "gender", "color"];
+  const values = ["Skitty", "cat", 9, "female", "tabby"];
+  // your code here
+  var result = keys.map(function(value, index) {
+    return [keys[index], values[index]];
+  });
+
+  document.getElementById("run").addEventListener("click", function() {
+    const obj = Object.fromEntries(result);
+    console.log(obj);
+    });
+
 })();
